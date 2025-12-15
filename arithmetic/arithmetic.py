1e979f78841418c3bb83044bef346c5ea83f31c5
deekshithsai16 <deekshithsai08@cloud.com>
2025-12-15 20:09:14 +0530

    op

---
+++ arithmetic/arithmetic.py
@@ -0,0 +1,22 @@
+num_1=256
+num_2=20.5
+num_3=3+5j
+num_4=25
+print("num_1 : ",num_1)
+print("num_2 : ",num_2)
+print("num_3 : ",num_3)
+print("num_4 : ",num_4)
+sum=num_1+num_2
+print("sum of num_1+num_2 is : ",sum)
+sub=num_1-num_3
+print("sub of num_1-num_3 is : ",sub)
+mul=num_1*num_3
+print("mul of num_1*num_3 is : ",mul)
+div=num_1/num_2
+print("div of num_1/num_2 is : ",div)
+mod=num_1%num_4
+print("mod of num_1%num_4 is : ",mod)
+exp=num_1**num_4
+print("mod of num_1**num_4 is : ",exp)
+flo=num_1//num_4
+print("flo of num_1//num_4 is : ",flo)
---
+++ assignment/assignment.py
@@ -0,0 +1,18 @@
+num_1=3125
+print("value of num_1 is : ",num_1)
+num_1 +=10
+print("value of num_1 is : ",num_1)
+num_1 -=10
+print("value of num_1 is : ",num_1)
+num_1 *=10
+print("value of num_1 is : ",num_1)
+num_1 /=10
+print("value of num_1 is : ",num_1)
+num_1 %=10
+print("value of num_1 is : ",num_1)
+print(int(num_1))
+num_1 //=10
+print("value of num_1 is : ",num_1)
+num_1=1234
+num_1 **=3
+print("value of num_1 is : ",num_1)
