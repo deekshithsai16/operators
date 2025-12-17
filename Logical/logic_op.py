d720be81f62d51472297b09c1e6776203ff1da24
deekshithsai16 <deekshithsai08@cloud.com>
2025-12-17 19:25:54 +0530

    't'

---
+++ Logical/logic_op.py
@@ -0,0 +1,38 @@
+#logical operators on two persons about their weight and height
+sd=[5.8,60]		#sd<siddharth>
+ba=[6.0,75]		#ba<baladitya>
+stmt_1=sd[0]>ba[0]
+stmt_2=sd[1]>ba[1]
+print(stmt_1)
+print(stmt_2)
+res=stmt_1 and stmt_2
+print("your assemption is:",res)
+
+stmt_1=sd[0]<ba[0]
+stmt_2=sd[1]<ba[1]
+print(stmt_1)
+print(stmt_2)
+re=stmt_1 and stmt_2
+print("your assemption is:",re)
+
+stmt_1=sd[0]>ba[0]
+stmt_2=sd[1]<ba[1]
+print(stmt_1)
+print(stmt_2)
+resu=stmt_1 and stmt_2
+print("your assemption is:",resu)
+
+stmt_1=sd[0]<ba[0]
+stmt_2=sd[1]>ba[1]
+print(stmt_1)
+print(stmt_2)
+result=stmt_1 and stmt_2
+print("your assemption is:",result)
+
+
+
+
+
+
+
+
