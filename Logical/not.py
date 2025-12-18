fe97db6af40d301819341076561d8dac93d66489
deekshithsai16 <deekshithsai08@cloud.com>
2025-12-18 20:03:00 +0530

    'o'

---
+++ Logical/not.py
@@ -0,0 +1,9 @@
+stmt_1=True
+stmt_2=False
+print(stmt_1)
+res=not stmt_1
+print(res)
+
+print(stmt_2)
+res=not stmt_2
+print(res)
---
+++ bitwise/bit.py
@@ -0,0 +1,67 @@
+#And operator
+
+num_1=15
+num_2=90
+print("num_1 value is : ",num_1)
+print("num_2 value is : ",num_2)
+print("binary form of num_1 is : ",bin(num_1))
+print("binary form of num_2 is : ",bin(num_2))
+res=num_1 & num_2
+print(res)
+
+#Or operator
+
+num_1=300
+num_2=909
+print("num_1 value is : ",num_1)
+print("num_2 value is : ",num_2)
+res=num_1 | num_2
+print(res)
+
+#Xor operator
+
+
+num_1=15
+num_2=90
+print("num_1 value is : ",num_1)
+print("num_2 value is : ",num_2)
+res=num_1 ^ num_2
+print(res)
+
+#Not operator
+
+num_1=70
+print("num_1 value is : ",num_1)
+res=~(num_1)
+print(res)
+
+#right shift
+
+num_1=60
+print("num_1 value is : ",num_1)
+res=num_1>>3
+print(res)
+print("binary form of res is : ",bin(res))
+
+num_2='0b1111000000'
+print(int(num_2,2))
+
+#left shift
+num_1=60
+print("num_1 value is : ",num_1)
+res=num_1<<4
+print(res)
+print("binary form of res is : ",bin(res))
+
+
+
+
+
+
+
+
+
+
+
+
+
